b95e559cfcbb343634620dd171f1df1d356aa859
Dioguinxz <doassis@hotmail.com.br>
2024-08-23 13:51:36 -0300

    Criação e implementação da classe CustomUsuarioDetailsService

---
+++ src/main/java/ProjetoTCC/TCC2/security/CustomUsuarioDetailsService.java
@@ -0,0 +1,21 @@
+package ProjetoTCC.TCC2.security;
+
+import ProjetoTCC.TCC2.entity.Usuario;
+import ProjetoTCC.TCC2.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+
+public class CustomUsuarioDetailsService implements UserDetailsService {
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Usuario usuario = this.repository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(usuario.getEmail(), usuario.getSenha(), new ArrayList<>());
+    }
+}
