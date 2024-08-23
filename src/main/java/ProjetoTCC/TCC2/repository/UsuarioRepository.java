f5b74d5263780837db351dad5d7f5ec4999c0f1b
Dioguinxz <doassis@hotmail.com.br>
2024-08-23 13:45:17 -0300

    Criação do método 'findByEmail' na classe UsuarioRepository

--- src/main/java/ProjetoTCC/TCC2/repository/UsuarioRepository.java
+++ src/main/java/ProjetoTCC/TCC2/repository/UsuarioRepository.java
@@ -3,6 +3,9 @@ package ProjetoTCC.TCC2.repository;
 import ProjetoTCC.TCC2.entity.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends MongoRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 
 }
