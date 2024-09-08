ca1bbb0d73a7c0d09314bcc00d8629739b3a9278
MorganaPithan <morgana.dpithan@gmail.com>
2024-09-08 17:08:41 -0300

    relacionando tarefas com usuario

--- src/main/java/ProjetoTCC/TCC2/service/TarefaService.java
+++ src/main/java/ProjetoTCC/TCC2/service/TarefaService.java
@@ -1,27 +1,45 @@
 package ProjetoTCC.TCC2.service;
 
 import ProjetoTCC.TCC2.entity.Tarefa;
+import ProjetoTCC.TCC2.entity.Usuario;
 import ProjetoTCC.TCC2.repository.TarefaRepository;
+import ProjetoTCC.TCC2.repository.UsuarioRepository;
 import org.bson.types.ObjectId;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class TarefaService {
     private TarefaRepository tarefaRepository;
+    private UsuarioRepository usuarioRepository;
 
-    public TarefaService(TarefaRepository tarefaRepository) {
+    public TarefaService(TarefaRepository tarefaRepository, UsuarioRepository usuarioRepository) {
         this.tarefaRepository = tarefaRepository;
+        this.usuarioRepository = usuarioRepository;
     }
 
     public List<Tarefa> criarTarefa(Tarefa tarefa) {
+
+        tarefa.validate();
+
+        Usuario usuario = usuarioRepository.findById(tarefa.getUsuarioId())
+                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado com o ID: " + tarefa.getUsuarioId()));
+
+        if (usuario.getTarefas() == null) {
+            usuario.setTarefas(new ArrayList<>());
+        }
+        usuario.getTarefas().add(tarefa);
+
         tarefaRepository.save(tarefa);
+        usuarioRepository.save(usuario);
+
         return listarTarefa();
     }
 
-        public List<Tarefa> listarTarefa() {
+    public List<Tarefa> listarTarefa() {
         Sort sort = Sort.by("nome").ascending();
         return tarefaRepository.findAll(sort);
     }
