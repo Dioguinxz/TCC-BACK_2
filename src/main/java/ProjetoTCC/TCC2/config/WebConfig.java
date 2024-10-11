1d285e4174443f88841a2d914b71b6b09ec18536
MorganaPithan <morgana.dpithan@gmail.com>
2024-10-10 22:11:22 -0300

    Webconfig atualizado

--- src/main/java/ProjetoTCC/TCC2/config/WebConfig.java
+++ src/main/java/ProjetoTCC/TCC2/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://simpletcc.com/")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
